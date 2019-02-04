47bdc2f5829d932886ee135990b8edf8f119ea0e
Ross <s1424268@ed.ac.uk>
2019-02-04 19:46:01 +0000

    home page message

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
@@ -9,7 +9,13 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'Bob'
 api = Api(app)
 
+
+class home(Resource):
+    def get(self):
+        return render_template("home.html")
+
 if __name__ =='__main__':
     from db import db
     db.init_app(app)
     app.run(debug=True)
+
