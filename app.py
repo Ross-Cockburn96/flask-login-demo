bc700bca899a0feedb88a97d96144213480244a4
Ross <s1424268@ed.ac.uk>
2019-02-04 20:03:14 +0000

    bug fix

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite://data.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///data.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'Bob'
