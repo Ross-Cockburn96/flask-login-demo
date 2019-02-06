12221ebf6a2b795edf7244236ac3f023168ef3c7
Ross <s1424268@ed.ac.uk>
2019-02-06 09:45:38 +0000

    removed typo

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from models.tag import TagModel
 
 app = Flask(__name__)
 CORS(app)
-app.config["Access-Control-Allow-Origin"]
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///data.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
